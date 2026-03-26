b47032565b01c7a0b85a252cad8e80924be95691
김해찬 <gibbm1127@naver.com>
2026-03-26 20:55:12 +0900

    init

---
+++ src/main/java/ssafy/study/ssafystudy/SsafyStudyApplication.java
@@ -0,0 +1,13 @@
+package ssafy.study.ssafystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafyStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafyStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssafy/study/ssafystudy/SsafyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy.study.ssafystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
