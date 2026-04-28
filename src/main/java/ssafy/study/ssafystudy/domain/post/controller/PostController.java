9b9e39196fd1e21b0a5edde79bf57c229e6e4775
k-haechan <gibbm1127@naver.com>
2026-04-28 10:54:36 +0900

    refactor: 게시글 생성, 삭제, 수정 시에 인증 확인로직 추가

--- src/main/java/ssafy/study/ssafystudy/domain/post/controller/PostController.java
+++ src/main/java/ssafy/study/ssafystudy/domain/post/controller/PostController.java
@@ -3,9 +3,13 @@ package ssafy.study.ssafystudy.domain.post.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ssafy.study.ssafystudy.domain.auth.component.SessionManager;
+import ssafy.study.ssafystudy.domain.auth.util.AuthTokenUtils;
 import ssafy.study.ssafystudy.domain.post.controller.dto.PostRequest;
 import ssafy.study.ssafystudy.domain.post.controller.dto.PostResponse;
 import ssafy.study.ssafystudy.domain.post.service.PostService;
+import ssafy.study.ssafystudy.global.exception.CustomException;
+import ssafy.study.ssafystudy.global.exception.error.ErrorCode;
 
 import java.util.List;
 
@@ -15,11 +19,18 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+    private final SessionManager sessionManager;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public PostResponse createPost(@RequestBody PostRequest request) {
-        return postService.create(request);
+    public PostResponse createPost(@RequestHeader("Authorization") String bearerToken, @RequestBody PostRequest request) {
+        if(AuthTokenUtils.isValidBearerToken(bearerToken)) {
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+        String sessionKey = AuthTokenUtils.parseBearerToken(bearerToken);
+        Long authorId = sessionManager.getMemberId(sessionKey);
+
+        return postService.create(request, authorId);
     }
 
     @GetMapping
@@ -36,14 +47,28 @@ public class PostController {
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public PostResponse updatePost(@PathVariable Long id, @RequestBody PostRequest request) {
-        return postService.update(request, id);
+    public PostResponse updatePost(@PathVariable Long id,
+                                   @RequestHeader("Authorization") String bearerToken,
+                                   @RequestBody PostRequest request) {
+        if(AuthTokenUtils.isValidBearerToken(bearerToken)) {
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+        String sessionKey = AuthTokenUtils.parseBearerToken(bearerToken);
+        Long authorId = sessionManager.getMemberId(sessionKey);
+        return postService.update(request, id, authorId);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletePost(@PathVariable Long id) {
-        postService.delete(id);
+    public void deletePost(@PathVariable Long id,
+                           @RequestHeader("Authorization") String bearerToken
+    ) {
+        if(AuthTokenUtils.isValidBearerToken(bearerToken)) {
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+        String sessionKey = AuthTokenUtils.parseBearerToken(bearerToken);
+        Long authorId = sessionManager.getMemberId(sessionKey);
+        postService.delete(id,authorId);
     }
 
 }
